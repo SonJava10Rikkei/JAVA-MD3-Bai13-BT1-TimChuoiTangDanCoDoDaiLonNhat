eb18fe857267db87df9ecbcff265e61f2f17a3b5
SonJava10Rikkei <sonjapan7@gmail.com>
2023-03-23 11:32:25 +0700

    Tìm chuỗi tăng dần có độ dài lớn nhất

---
+++ src/config/Config.java
@@ -0,0 +1,12 @@
+package config;
+
+import java.util.Scanner;
+
+public class Config {
+    public static Scanner scanner() {
+//        Scanner scanner = new Scanner(System.in);
+//        return scanner;
+        return new Scanner(System.in);
+    }
+
+}
